5f8cb255aebe55b4954f44b4ec864223911e8956
Gaby <gaabyrocha574@gmail.com>
2021-10-21 19:43:13 +0000

    tarefa

---
+++ Pessoa.java
@@ -0,0 +1,22 @@
+public class Pessoa{
+    private String nome;
+    private String telefone;
+    private String endereço;
+//fazer os métodos de acesso público
+//get e set
+public void setNome(String nome){
+    this.nome=nome;
+}
+
+    public String getNome(){
+        return this.nome;
+  }
+
+  //set
+  public void setTelefone(String telefone){
+      this.telefone=telefone;
+
+ 
+ 
+    }
+}
